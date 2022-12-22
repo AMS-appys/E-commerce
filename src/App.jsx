88efdf832992aad21665fe4a9e12a0af02e56bc6
AMS-appys <100517888+AMS-appys@users.noreply.github.com>
2022-12-22 13:17:32 +0530

    3 files modified, 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
 import './App.css'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
-  
-
   return (
     <div className="App">
-      Hello
+      <Navbar />
+      <Home />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+const Navbar = () =>
+
+const Container = styled.div`
+background color: black
+
+{
+  
+  return(
+    <div>Navbar </div>
+  )
+}
+export default Navbar
